e97b44eb649e5c6b84bcf9bbc98f904596a6eed0
Penn Shen <360346034@qq.com>
2018-01-18 23:11:23 +0800

    python3排序
    python3移除了sorted中的cmp函数。只有list，key，reserved三个了

--- sortedtest.py
+++ sortedtest.py
@@ -1,9 +1,2 @@
-def cmp_ignore_case(s1, s2):
-    n1 = s1.upper()
-    n2 = s2.upper()
-    if n1 > n2:
-        return 1
-    if n1 < n2:
-        return -1
-    return 0
-print (list(sorted(['bob','about','Zoo','Credit'],cmp_ignore_case)))
+
+print (list(sorted(['bob','about','Zoo','Credit'],key=lambda x : x.lower())))
